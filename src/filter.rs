0942d6a605c7a9609858a8717c5a23dc3c508b79
ae2rs <lucasdc@amo.co>
2026-03-26 18:13:16 +0100

    clippy

--- src/filter.rs
+++ src/filter.rs
@@ -12,11 +12,11 @@ impl Filter {
     /// Creates a new filter with 2^`size` bits, optimized for `n` expected items.
     pub fn new(size: usize, n: usize) -> Result<Self, Error> {
         if size > 63 {
-            return Err(Error::InvalidArgument(format!("max size is 63")));
+            return Err(Error::InvalidArgument("max size is 63".to_string()));
         }
 
         if n == 0 {
-            return Err(Error::InvalidArgument(format!("n must be greater than 0")));
+            return Err(Error::InvalidArgument("n must be greater than 0".to_string()));
         }
 
         let m = 1u64 << size;
