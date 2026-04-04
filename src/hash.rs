85486ba1f23d2c7880a7127ec8be2480bb7a02ed
ae2rs <lucasdc@amo.co>
2026-04-04 19:43:44 +0200

    simplify hash split

--- src/hash.rs
+++ src/hash.rs
@@ -2,11 +2,9 @@ use xxhash_rust::xxh3::xxh3_128;
 
 #[inline]
 pub fn hash(value: impl AsRef<[u8]>) -> (u64, u64) {
-    let h128 = xxh3_128(value.as_ref()).to_le_bytes();
-    let h1 = u64::from_le_bytes(h128[0..8].try_into().unwrap());
-    let mut h2 = u64::from_le_bytes(h128[8..16].try_into().unwrap());
-
+    let h128 = xxh3_128(value.as_ref());
+    let h1 = h128 as u64;
+    let mut h2 = (h128 >> 64) as u64;
     h2 |= 1;
-
     (h1, h2)
 }
